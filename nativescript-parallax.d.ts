14d2a0c4704240d84fb7e19b85ba8da58469a033
Steve McNiven-Scott <steve@sitefinitysteve.com>
2017-08-22 00:35:26 -0400

    3.1 update, add events, perf improvements, fix bounce event, update readme

--- nativescript-parallax.d.ts
+++ nativescript-parallax.d.ts
@@ -17,12 +17,17 @@ export declare class ParallaxView extends GridLayout {
     private _controlsToFade;
     private _childLayouts;
     private _includesAnchored;
+    private _isAnchored;
     private _topOpacity;
     private _loaded;
     private _minimumHeights;
     private _bounce;
+    static scrollEvent: string;
+    static anchoredEvent: string;
+    static unanchoredEvent: string;
     bounce: boolean;
     controlsToFade: string;
+    readonly isAnchored: boolean;
     readonly android: any;
     readonly ios: any;
     constructor();
